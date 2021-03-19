0fc497faa65512ec89df01adc835e7ee9f45773c
Replit user <replituser@example.com>
2021-03-19 19:25:50 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -101,8 +101,7 @@ Programe la función void asignarValor(Vector * v, int i, int valor),
 la cual asigna el valor a la posición i del vector v.
 */
 void asignarValor(Vector * v, int i, int valor) {
-  v->datos[i]=valor;
-  
+  v->datos[i]=valor; 
 }
 
 /*
@@ -111,11 +110,8 @@ Programe la función int obtenerValor(Vector * v, int i),
 la cual retorna el valor en la posición i del vector v.
 */
 int obtenerValor(Vector * v, int i) {
-
   int aux;
   aux=v->datos[i];
-  
-
    return aux;
 }
 
@@ -124,8 +120,11 @@ Ejercicio 7.
 Función que suma los vectores `a` y `b` y 
 actualiza el vector `c` con el resultado de la suma.
 */
-void sumaV(Vector * a, Vector * b, Vector * c) {
-
+void sumaV(Vector * a, Vector * b, Vector * c) { 
+int i;
+  for (i=0;i<3;i++){
+    c->datos[i]=a->datos[i]+b->datos[i];  
+  }
 }
 
 /*
